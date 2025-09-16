f0a0ef7cf298b018359e0393588fbac33aa7e784
rebeccanash <r.nash@imperial.ac.uk>
2025-09-16 10:11:34 +0100

    Add test for calculate_delay_boundaries

--- tests/testthat/test-mcmc-initialise.R
+++ tests/testthat/test-mcmc-initialise.R
@@ -1,3 +1,5 @@
+# test calculate_transitive_steps()
+
 test_that("calculate_transitive_steps works correctly", {
 
   delay_map <- data.frame(
@@ -20,4 +22,34 @@ test_that("calculate_transitive_steps works correctly", {
   
   expect_equal(actual_output, expected_output)
   
+})
+
+# test calculate_delay_boundaries()
+
+test_that("calculate_delay_boundaries estimates correct quantiles", {
+
+delay_params <- data.frame(
+  group = c(1:4, 2, 3, 3, 4, 4),
+  from = c("onset", "onset", "onset", "onset", "onset", "onset",
+           "hospitalisation", "onset", "hospitalisation"),
+  to = c("report", "report", "report", "report", "death", "hospitalisation",
+         "discharge", "hospitalisation", "death"),
+  delay_mean = c(10, 10, 10, 10, 15, 7, 20, 7, 12),
+  delay_cv = c(0.3, 0.3, 0.3, 0.3, 0.4, 0.2, 0.5, 0.2, 0.3)
+)
+
+quantile_range <- c(0.01, 0.99)
+
+shape <- (1 / delay_params$delay_cv)^2
+scale <- delay_params$delay_mean / shape
+exp_min <- qgamma(quantile_range[1], shape = shape, scale = scale)
+exp_max <- qgamma(quantile_range[2], shape = shape, scale = scale)
+
+actual <- calculate_delay_boundaries(delay_params, quantile_range)
+act_min <- actual$min_delay
+act_max <- actual$max_delay
+
+expect_equal(exp_min, act_min)
+expect_equal(exp_max, act_max)
+
 })
