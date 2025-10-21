362c9f69374c368407086878a8279a0bf9f51474
edknock <edwardknock@gmail.com>
2025-10-21 13:56:30 +0100

    Add dim2, date_to_int and int_to_date work on arrays

--- R/util.R
+++ R/util.R
@@ -1,13 +1,17 @@
 date_to_int <- function(date, origin = "1970-01-01") {
-  as.integer(as.Date(date) - as.Date(origin))
+  array(as.integer(as.Date(date) - as.Date(origin)), dim2(date))
 }
 
 
 int_to_date <- function(int, origin = "1970-01-01") {
-  int + as.Date(origin)
+  array(int + as.Date(origin), dim2(int))
 }
 
 
 data_frame_to_array <- function(df) {
-  array(df, dim(df))
+  array(unlist(df), dim(df))
+}
+
+dim2 <- function(x) {
+  dim(x) %||% length(x)
 }
