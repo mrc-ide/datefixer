47563aaa79be41a64a3d470d75b9fa71b478138a
edknock <edwardknock@gmail.com>
2025-11-11 11:02:50 +0000

    new setup

--- R/mcmc-augmented-data-update.R
+++ R/mcmc-augmented-data-update.R
@@ -5,7 +5,7 @@ update_augmented_data <- function(augmented_data, observed_dates, pars, groups,
     augmented_data_i <- 
       update_augmented_data1(augmented_data$estimated_dates[i, ],
                              augmented_data$error_indicators[i, ],
-                             observed_dates[i, ], pars, groups[i], delay_map,
+                             observed_dates[i, ], pars, groups[i], delay_info,
                              control, rng)
     augmented_data$estimated_dates[i, ] <- augmented_data_i$estimated_dates
     augmented_data$error_indicators[i, ] <- augmented_data_i$error_indicators
@@ -20,7 +20,7 @@ update_augmented_data1 <- function(estimated_dates, error_indicators,
   
   estimated_dates <- update_estimated_dates(estimated_dates, error_indicators,
                                             observed_dates, pars, group,
-                                            delay_map, control, rng)
+                                            delay_info, control, rng)
   
   list(estimated_dates = estimated_dates,
        error_indicators = error_indicators)
@@ -38,7 +38,7 @@ update_estimated_dates <- function(estimated_dates, error_indicators,
     estimated_dates <- update_estimated_dates1(i, estimated_dates,
                                                error_indicators, observed_dates,
                                                mean_delays, cv_delays, group,
-                                               delay_map, control, rng)
+                                               delay_info, control, rng)
   }
   
   estimated_dates
@@ -52,18 +52,17 @@ update_estimated_dates1 <- function(i, estimated_dates, error_indicators,
   is_delay_in_group <- delay_info$is_delay_in_group[, group]
   ## TRUE/FALSE is date i for the given group involved in each delay
   is_date_in_delay <- is_delay_in_group &
-    (i == delay_map$from | i == delay_map$to)
+    (i == delay_info$from | i == delay_info$to)
   
   if (!any(is_date_in_delay)) {
+    ## date is not associated with any delays, so no update
     return(estimated_dates)
   }
   
-  ## current_l
-  ll_current <- 
-    datefixer_log_likelihood_delays1(
-      array(estimated_dates, c(1, length(estimated_dates))),
-            mean_delays, cv_delays, delay_info$from, delay_info$to,
-            is_delay_in_group)
+  ## current log likelihood
+  ll_current <- datefixer_log_likelihood_delays1(
+    estimated_dates, mean_delays, cv_delays, delay_info$from, delay_info$to,
+    is_delay_in_group)
   
   if (is.na(error_indicators[i])) {
     ## update missing date
@@ -72,4 +71,6 @@ update_estimated_dates1 <- function(i, estimated_dates, error_indicators,
   } else {
     ## update non-error date
   }
+  
+  estimated_dates
 }
