dc785e04dab212cc02eb55a9d88b34783d2cb85d
edknock <edwardknock@gmail.com>
2025-12-02 13:30:00 +0000

    move comment back

--- R/mcmc-augmented-data-update.R
+++ R/mcmc-augmented-data-update.R
@@ -122,8 +122,9 @@ update_error_indicators1 <- function(i, augmented_data, observed_dates, group,
   is_date_in_delay <- is_delay_in_group &
     (i == delay_info$from | i == delay_info$to)
   
-  ## date is not associated with any delays for that group, so no update
+  
   if (!any(is_date_in_delay)) {
+    ## date is not associated with any delays for that group, so no update
     return(augmented_data)  
   } 
   
